62629daabd54ca9f45a37b860a1c3acb2e9d453c
Vadim Kostrov <vmkvo15@outlook.com>
2021-11-16 16:23:30 +0000

    Created createDiv function

---
+++ script.js
@@ -0,0 +1,13 @@
+const container = document.querySelector('.grid-container');
+
+function createDiv() {
+    const div = document.createElement('div');
+    container.appendChild(div);
+}
+
+function createGrid(x, y) {
+
+
+
+
+}
