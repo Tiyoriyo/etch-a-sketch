0950079c07d361f4495af7527c81bec9270376fb
Vadim Kostrov <vmkvo15@outlook.com>
2021-11-16 23:13:38 +0000

    Created createGrid function

--- script.js
+++ script.js
@@ -1,13 +1,42 @@
+const divs = document.querySelectorAll('div');
 const container = document.querySelector('.grid-container');
+const divSquare = document.createElement('div');
+const divLineContainer = document.createElement('div');
 
-function createDiv() {
-    const div = document.createElement('div');
-    container.appendChild(div);
+function createDivSquare() {
+    divSquare.classList.add('square')
+    return;
 }
 
-function createGrid(x, y) {
+function createDivContainer(width) {
+    divLineContainer.classList.add('line-container');
+    container.appendChild(divLineContainer);
 
+    createDivSquare();
+    divLineContainer.appendChild(divSquare);
 
-
-
+    for (i = 0; i < width-1; i++) {
+        divLineContainer.appendChild(divSquare.cloneNode(true));
+    }
 }
+
+
+function createGrid(width) {
+    createDivContainer(width);
+    container.appendChild(divLineContainer);
+
+    for (i = 0; i < width-1; i++) {
+        container.appendChild(divLineContainer.cloneNode(true));
+    }
+}
+
+/*
+function paintRed(e) {
+    e.stopPropagation();
+    e.classList.add('red');
+}
+
+divs.forEach(div => {
+    addEventListener('mouseover', paintRed);
+});
+*/
