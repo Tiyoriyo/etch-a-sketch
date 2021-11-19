4e5e11721510494d16911056b1f57dfcfcd26e0c
Vadim Kostrov <vmkvo15@outlook.com>
2021-11-19 19:16:38 +0000

    Restructured code

--- script.js
+++ script.js
@@ -1,42 +1,63 @@
 const divs = document.querySelectorAll('div');
-const container = document.querySelector('.grid-container');
+const gridContainer = document.querySelector('#grid-container');
+const lineContainerAll = document.getElementsByClassName('.line-container')
+const square = document.getElementsByClassName('square');
+const clearButton = document.querySelector('#clear');
+
 const divSquare = document.createElement('div');
 const divLineContainer = document.createElement('div');
 
+createGrid(16);
+
+divs.forEach(() => {
+    addEventListener('mouseover', paintRed);
+});
+
+clearButton.addEventListener('click', () => {
+    addEventListener('click', newCanvas);
+});
+
+
+function clearCanvas() {
+    while (gridContainer.hasChildNodes()) {
+        gridContainer.removeChild(gridContainer.lastChild);
+    }
+}
+
+function newCanvas() {
+    const x = prompt('How many squares do you want per side?', 16);
+    if (x == null) {
+        return
+    } else if (x > 100 || x < 1 || x == NaN) {
+        alert('You must pick a number from 1 to 100.');
+        return newCanvas();
+    } else {
+        clearCanvas();
+        createGrid(x);
+    }
+}
+
 function createDivSquare() {
-    divSquare.classList.add('square')
+    divSquare.classList.add('square');
     return;
 }
 
-function createDivContainer(width) {
-    divLineContainer.classList.add('line-container');
-    container.appendChild(divLineContainer);
-
-    createDivSquare();
-    divLineContainer.appendChild(divSquare);
-
-    for (i = 0; i < width-1; i++) {
-        divLineContainer.appendChild(divSquare.cloneNode(true));
+function createGrid(x) {
+    let totalBox = x ** 2;
+    let boxWidth = 500 / x;
+    
+    for (i = 0; i < totalBox; i++) {
+        const div = document.createElement('div');
+        div.setAttribute('id', 'box');
+        div.style.setProperty('width', boxWidth + "px");
+        gridContainer.appendChild(div);
     }
 }
 
-
-function createGrid(width) {
-    createDivContainer(width);
-    container.appendChild(divLineContainer);
-
-    for (i = 0; i < width-1; i++) {
-        container.appendChild(divLineContainer.cloneNode(true));
-    }
-}
-
-/*
 function paintRed(e) {
-    e.stopPropagation();
-    e.classList.add('red');
+    if (e.target.id == 'box') {
+        e.target.classList.add('red');
+    } else {
+        return;
+    }
 }
-
-divs.forEach(div => {
-    addEventListener('mouseover', paintRed);
-});
-*/
