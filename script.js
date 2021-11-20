2509d57d29a4bd18a43f2ddbb20a769fa4b0d12f
Vadim Kostrov <vmkvo15@outlook.com>
2021-11-20 22:20:44 +0000

    Updated function paintRGB to getRGB

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ const blue = document.querySelector('#blue');
 const rgb = document.querySelector('#rgb');
 const black = document.querySelector('#black');
 
+let colours = ['#ff0000', '#ff9a00', '#d0de21', '#4fde21', '#3fdad8', '#2fc9e2', '#1c7fee', '#5f15f2', '#ba0cf8', '#ba0cf8', '#fb07d9']
+
+let count = 0;
+
 const divSquare = document.createElement('div');
 const divLineContainer = document.createElement('div');
 
@@ -17,6 +21,7 @@ clearButton.addEventListener('click', newCanvas);
 
 blue.addEventListener('click', () => {
     divs.forEach(() => {
+        removeEventListener('mouseover', setRgbClass);
         addEventListener('mouseover', paintBlue);
     });
 });
@@ -59,8 +64,50 @@ function createGrid(x) {
 
 function paintBlue(e) {
     if (e.target.id == 'box') {
-        e.target.classList.add('blue');
+        e.target.style.setProperty('background-color', '#0693cf')
     } else {
         return;
     }
 }
+
+function getRGB() {
+    let colours = ['#ff0000', '#ff9a00', '#d0de21', '#4fde21', '#3fdad8', '#2fc9e2', '#1c7fee', '#5f15f2', '#ba0cf8', '#ba0cf8', '#fb07d9']
+    let index; 
+    let remainderDiv = colours.length;
+
+    if (count > remainderDiv) {
+        index = (count % remainderDiv) - 1;
+        count++;
+    } else if (count == remainderDiv) {
+        index = remainderDiv - 1;
+        count++;
+    } else if (count < remainderDiv && count >= 0) {
+        index = count;
+        count++;
+    } 
+
+    return colours[index];
+}
+
+function setRgbClass(e) {
+    if (e.target.id == 'box') {
+        e.target.style.setProperty('background-color', getRGB());
+    } else {
+        return;
+    }
+}
+
+/*
+const colors = {
+    red: [255, 0, 0],
+    orange: [255, 154, 0],
+    yellow: [208, 222, 33],
+    green: [79, 222, 33],
+    turquoise: [63, 218, 216],
+    lightBlue: [47, 201, 226], 
+    blue: [28, 127, 238],
+    darkBlue: [95, 21, 242],
+    purple: [186, 12, 248],
+    pink: [251, 7, 217],
+}
+*/
