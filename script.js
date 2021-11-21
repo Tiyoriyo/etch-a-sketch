fd3b675bc5eba9cbbd466fbd3715cf84bb030e60
Vadim Kostrov <vmkvo15@outlook.com>
2021-11-21 14:21:42 +0000

    Added paintBlack function

--- script.js
+++ script.js
@@ -7,8 +7,6 @@ const blue = document.querySelector('#blue');
 const rgb = document.querySelector('#rgb');
 const black = document.querySelector('#black');
 
-let colours = ['#ff0000', '#ff9a00', '#d0de21', '#4fde21', '#3fdad8', '#2fc9e2', '#1c7fee', '#5f15f2', '#ba0cf8', '#ba0cf8', '#fb07d9']
-
 let count = 0;
 
 const divSquare = document.createElement('div');
@@ -22,7 +20,24 @@ clearButton.addEventListener('click', newCanvas);
 blue.addEventListener('click', () => {
     divs.forEach(() => {
         removeEventListener('mouseover', setRgbClass);
+        removeEventListener('mouseover', paintBlack);
         addEventListener('mouseover', paintBlue);
+    });
+});
+
+rgb.addEventListener('click', () => {
+    divs.forEach(() => {
+        removeEventListener('mouseover', paintBlue);
+        removeEventListener('mouseover', paintBlack);
+        addEventListener('mouseover', setRgbClass);
+    });
+});
+
+black.addEventListener('click', () => {
+    divs.forEach(() => {
+        removeEventListener('mouseover', paintBlue);
+        removeEventListener('mouseover', setRgbClass);
+        addEventListener('mouseover', paintBlack);
     });
 });
 
@@ -65,49 +80,43 @@ function createGrid(x) {
 function paintBlue(e) {
     if (e.target.id == 'box') {
         e.target.style.setProperty('background-color', '#0693cf')
+        e.target.style.opacity = 1;
     } else {
         return;
     }
 }
 
 function getRGB() {
-    let colours = ['#ff0000', '#ff9a00', '#d0de21', '#4fde21', '#3fdad8', '#2fc9e2', '#1c7fee', '#5f15f2', '#ba0cf8', '#ba0cf8', '#fb07d9']
+    let colours = ['#ff0000', '#ff9a00', '#d0de21', '#4fde21', '#3fdad8', '#2fc9e2', '#1c7fee', '#5f15f2', '#ba0cf8', '#fb07d9']
     let index; 
     let remainderDiv = colours.length;
 
-    if (count > remainderDiv) {
-        index = (count % remainderDiv) - 1;
-        count++;
-    } else if (count == remainderDiv) {
-        index = remainderDiv - 1;
+    if (count >= remainderDiv) {
+        index = (count % remainderDiv);
         count++;
     } else if (count < remainderDiv && count >= 0) {
         index = count;
         count++;
     } 
-
     return colours[index];
 }
 
 function setRgbClass(e) {
     if (e.target.id == 'box') {
         e.target.style.setProperty('background-color', getRGB());
+        e.target.style.opacity = 1;
     } else {
         return;
     }
 }
 
-/*
-const colors = {
-    red: [255, 0, 0],
-    orange: [255, 154, 0],
-    yellow: [208, 222, 33],
-    green: [79, 222, 33],
-    turquoise: [63, 218, 216],
-    lightBlue: [47, 201, 226], 
-    blue: [28, 127, 238],
-    darkBlue: [95, 21, 242],
-    purple: [186, 12, 248],
-    pink: [251, 7, 217],
+function paintBlack(e) {    
+    if (e.target.id == 'box') {
+        if (e.target.style.backgroundColor !== 'black') {
+            e.target.style.setProperty('background-color', 'black');
+            e.target.style.opacity = 0.33;
+        } else {
+            e.target.style.opacity = +e.target.style.opacity + 0.10;
+        }
+    }
 }
-*/
